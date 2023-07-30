926cec350db862678169f35f0b031765e590cfa2
Ritesh Solanki <okayritesh@gmail.com>
2023-07-30 21:12:59 +0530

    Data

---
+++ src/Pages/AddProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddProduct(){
+    return(
+        <React.Fragment>
+            <div>Hello</div>
+        </React.Fragment>
+    )
+}
+
+export default AddProduct;
---
+++ src/Pages/UpdataProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UpdatProduct(){
+    return(
+        <React.Fragment>
+            <div>Hello</div>
+        </React.Fragment>
+    )
+}
+
+export default UpdatProduct;
